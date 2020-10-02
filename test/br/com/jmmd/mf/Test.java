1460ef3819c25555bad589bfc93b1295efd6d57b
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-10-02 15:52:00 -0300

    Starting a project

---
+++ test/br/com/jmmd/mf/Test.java
@@ -0,0 +1,15 @@
+package br.com.jmmd.mf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void testequalstwo() {
+		
+		int a = 1+1;
+		assertEquals(2, a);
+		
+	}
+
+}
